b2427e45e353ce69915c2fb182718adbe964d67c
Obada <obada8411@gmail.com>
2023-04-28 04:48:29 +0300

    Remove or disable _id from address schema

--- src/models/schemas/address.ts
+++ src/models/schemas/address.ts
@@ -1,15 +1,18 @@
 import { Schema } from "mongoose";
 
-const addressSchema = new Schema({
-  lat: { type: String },
-  lon: { type: String },
-  house_number: { type: String },
-  road: { type: String },
-  neighbourhood: { type: String },
-  suburb: { type: String },
-  city: { type: String },
-  state: { type: String },
-  country: { type: String },
-});
+const addressSchema = new Schema(
+  {
+    lat: { type: String },
+    lon: { type: String },
+    house_number: { type: String },
+    road: { type: String },
+    neighbourhood: { type: String },
+    suburb: { type: String },
+    city: { type: String },
+    state: { type: String },
+    country: { type: String },
+  },
+  { _id: false }
+);
 
 export default addressSchema;
