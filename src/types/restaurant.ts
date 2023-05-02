3a3409c6ae4929b90a111337582ff554cc6b7c90
Obada <obada8411@gmail.com>
2023-05-02 23:09:29 +0300

    Remove 'restaurant' property from restaurant type

--- src/types/restaurant.ts
+++ src/types/restaurant.ts
@@ -4,7 +4,6 @@ export type Restaurant = {
   name: string;
   time: string;
   small_image_url: { light: string; dark: string };
-  restaurant: Partial<Restaurant>;
 };
 
 export interface IRestaurantDocument extends Document, Restaurant {}
